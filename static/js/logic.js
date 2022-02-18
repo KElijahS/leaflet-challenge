d660ca028751efb91bfea6bf43eeae7344c8c79c
Eli Souvannarath <Kounnathum@gmail.com>
2022-02-18 16:57:14 -0500

    final push

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,38 +1,30 @@
-//get data
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-//create layer groups to build later
 var earthquake = L.layerGroup();
 
-// Create the tile layer that will be the background of our map.
 var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-// Create a baseMaps object.
 var baseMaps = {
   "Street Map": streetmap,
 };
 
-// Create an overlays object to add to the layer control
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5,
     layers:[streetmap, earthquake]
   });
 
- // Pass our map layers to our layer control.
-// Add the layer control to the map.
+
 L.control.layers(baseMaps, overlays, {
   collapsed: false
 }).addTo(myMap); 
 
-// Create an overlay object.
 var overlays = {
   "Earthquake": earthquake,
 };
 
-// Create a legend to display information about our map.
 var legend = L.control({
   position: "bottomright"
 });
@@ -80,11 +72,10 @@ d3.json(url).then(function(data) {
     earthquake.addTo(myMap)
   })
 
-    // Create a legend to display information about our map.
     var legend = L.control({
         position: "bottomright"
     });
-    // When the layer control is added, insert a div with the class of "legend".
+
     legend.onAdd = function(map) {
         var div = L.DomUtil.create("div", "info legend");
         var intensity =[-10,10,30,50,70,90];
